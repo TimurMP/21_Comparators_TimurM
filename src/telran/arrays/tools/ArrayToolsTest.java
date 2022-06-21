a2c3eb5119fc51782467601731525ee44f0537a3
TimurMP <titimurmamyshev@gmail.commur>
2022-06-22 00:04:44 +0300

    FFS - Done!

--- src/telran/arrays/tools/ArrayToolsTest.java
+++ src/telran/arrays/tools/ArrayToolsTest.java
@@ -8,11 +8,15 @@ import static org.junit.jupiter.api.Assertions.*;
 class ArrayToolsTest {
     Integer[] arrNum;
     String[] arrStr;
+    Integer[] origin;
+
 
     @BeforeEach
     void setUp() {
         arrNum = new Integer[]{9, 7, 4, 7, 2, 5, 9, 1, 0};
-        arrStr = new String[]{"one", "two", "three", "four", "five"};
+        arrStr = new String[] {"one", "two", "three", "four", "five"};
+        origin = new Integer[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 3};
+
     }
 
     @Test
@@ -51,9 +55,43 @@ class ArrayToolsTest {
 
     @Test
     void testBubbleSortStrComparator() {
-        System.out.println("Test BubbleSortString Comparator");
+        System.out.println("Test Bubble Sort String Comparator");
         ArrayTools.bubbleSort(arrStr, (s1, s2) -> s1.length() - s2.length());
         ArrayTools.printArray(arrStr);
+
+    }
+
+    @Test
+    void OddEvenComparator() {
+        System.out.println("Test Bubble Sort String Odd Even Comparator");
+        ArrayTools.bubbleSort(origin, (i1, i2) -> {
+
+            int even = i1 % 2;
+            int odd = i2 - i1;
+            if (i1 % 2 == 0){
+                return even;
+
+            }
+            return odd;
+        });
+        ArrayTools.printArray(origin);
+
+
+//        ArrayTools.bubbleSort(origin, (s1, s2) -> s1.length() - s2.length());
+//        origin = new Integer[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 3};
+
+//        ArrayTools.bubbleSort(origin, (i1, i2) -> {
+//            int countEven = 0;
+//            int countOdd = 0;
+//            int even = 0;
+//            if (i1 % 2 == 0){       }
+//
+//
+//
+//             });
+//            }}
+
+
 
     }
 
