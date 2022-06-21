162183ad22cd6fa805f6d37b3578e3719c2162d7
TimurMP <titimurmamyshev@gmail.commur>
2022-06-21 22:59:11 +0300

    Base

--- src/telran/arrays/tools/ArrayTools.java
+++ src/telran/arrays/tools/ArrayTools.java
@@ -1,2 +1,55 @@
-package telran.arrays.tools;public class ArrayTools {
+package telran.arrays.tools;
+
+import java.util.Comparator;
+import java.util.function.Predicate;
+
+public class ArrayTools {
+    public static void printArray(Object[] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+
+        }
+        System.out.println("==================***===================");
+
+    }
+
+
+    public static <T> T findFirst(T[] arr, Predicate<T> tester) {
+        for (int i = 0; i < arr.length; i++) {
+            if (tester.test(arr[i])) {
+                return arr[i];
+            }
+        }
+        return null;
+    }
+
+    public static <T extends Comparable<T>> void bubbleSort(T[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j].compareTo(arr[j + 1]) > 0) {
+                    T t = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = t;
+                }
+
+            }
+
+        }
+    }
+
+    public static <T> void bubbleSort(T[] arr, Comparator<T> comparator) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (comparator.compare(arr[j], arr[j+1]) > 0) {
+                    T t = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = t;
+                }
+
+            }
+
+        }
+    }
+
 }
--- src/telran/arrays/tools/ArrayToolsTest.java
+++ src/telran/arrays/tools/ArrayToolsTest.java
@@ -1,4 +1,60 @@
+package telran.arrays.tools;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ArrayToolsTest {
-  
+    Integer[] arrNum;
+    String[] arrStr;
+
+    @BeforeEach
+    void setUp() {
+        arrNum = new Integer[]{9, 7, 4, 7, 2, 5, 9, 1, 0};
+        arrStr = new String[]{"one", "two", "three", "four", "five"};
+    }
+
+    @Test
+    void testPrintArray() {
+        System.out.println("Test Print Array");
+        ArrayTools.printArray(arrNum);
+        ArrayTools.printArray(arrStr);
+    }
+
+    @Test
+    void testFindFirst() {
+        System.out.println("Test Find First");
+        Integer res = ArrayTools.findFirst(arrNum, n -> n % 2 == 0);
+        System.out.println(res);
+        String str = ArrayTools.findFirst(arrStr, s -> s.length() == 4);
+        System.out.println(str);
+    }
+
+
+    @Test
+    void testBubbleSortInt() {
+        System.out.println("Test Bubble Sort Integer");
+        ArrayTools.bubbleSort(arrNum);
+        ArrayTools.printArray(arrNum);
+
+
+    }
+
+    @Test
+    void testBubbleSortStr() {
+        System.out.println("Test Bubble Sort String");
+        ArrayTools.bubbleSort(arrStr);
+        ArrayTools.printArray(arrStr);
+
+    }
+
+    @Test
+    void testBubbleSortStrComparator() {
+        System.out.println("Test BubbleSortString Comparator");
+        ArrayTools.bubbleSort(arrStr, (s1, s2) -> s1.length() - s2.length());
+        ArrayTools.printArray(arrStr);
+
+    }
+
 }
